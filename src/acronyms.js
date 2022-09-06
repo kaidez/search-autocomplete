ffe568b007687f788f34f7cf942b33983a7869df
Kai Gittens <kai.gittens@gmail.com>
2022-09-06 15:56:25 -0400

    clean up code

--- src/acronyms.js
+++ src/acronyms.js
@@ -1,12 +1,12 @@
 import { fetchData, buildList } from './helpers';
 
 export const searchField = document.querySelector('#acronymSearchField');
-const searchResults = document.querySelector('#acronymSearchResults');
+export const searchResults = document.querySelector('#acronymSearchResults');
 
 export async function showAcronymSearchResults(searchTerm) {
   const acronyms = await fetchData('./acronyms.json');
   const acronymsList = buildList(acronyms, 'name');
-  let searchResultsListItem = '';
+  let searchResultsList = '';
   let regex = new RegExp(searchTerm, 'i');
 
   searchResults.innerHTML = '';
@@ -17,7 +17,7 @@ export async function showAcronymSearchResults(searchTerm) {
 
   return acronymsList.filter((term, idx) => {
     if (term.match(regex)) {
-      searchResultsListItem += '<li>' + term + '</li>';
+      searchResultsList += '<li>' + term + '</li>';
     }
     searchResults.innerHTML = '<ul>' + searchResultsList + '</ul>';
   });
