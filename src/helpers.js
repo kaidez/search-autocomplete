46310cf2349e6cd432578ae63ea6e737ecb5e53c
Kai Gittens <kai.gittens@gmail.com>
2022-09-06 11:37:15 -0400

    add comments

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-/* Helper methods...they should always be pure functions */
+// Helper methods...they should always be parametized pure functions
 
 // Fetch any data and format it as JSON
 export async function fetchData(url) {
@@ -9,5 +9,8 @@ export async function fetchData(url) {
 
 // Get the 'objectProperty' value from each item in the 'fetchedObject'
 // and place it in a standard JS array/list
+
+//Ex: buildList([{'age': 21}, {'age': '50'}], 'age') returns [21, 50]
+
 export const buildList = (fetchedObject, objectProperty) =>
   fetchedObject.map((item) => item[objectProperty]);
