b998836de8ebb012fd663aaab09f3d86890320b9
Kai Gittens <kai.gittens@gmail.com>
2022-09-07 16:44:30 -0400

    ad clearBtn

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ var acronyms_1 = require("./acronyms");
 acronyms_1.searchField.addEventListener('keyup', function () {
     (0, acronyms_1.showAcronymSearchResults)(acronyms_1.searchField.value);
 });
+acronyms_1.clearBtn.addEventListener('click', function () {
+    acronyms_1.searchField.value = '';
+});
