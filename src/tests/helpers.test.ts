bd6afb50e3e0eb476c957df3065e54a511c590c5
Kai Gittens <kai.gittens@gmail.com>
2022-09-11 13:00:23 -0400

    update helpers tests

--- src/tests/helpers.test.ts
+++ src/tests/helpers.test.ts
@@ -7,19 +7,21 @@ const testDataTwo = [
 ];
 
 describe('helpers.ts tests', () => {
-  test('buildList returns an array with one string', () => {
-    expect(buildList(testDataOne, 'name')).toStrictEqual(['joe']);
-  });
+  describe('buildList()', () => {
+    test('returns an array with one string', () => {
+      expect(buildList(testDataOne, 'name')).toStrictEqual(['joe']);
+    });
 
-  test('buildList returns an array with two strings', () => {
-    expect(buildList(testDataTwo, 'name')).toStrictEqual(['joe', 'jill']);
-  });
+    test('returns an array with two strings', () => {
+      expect(buildList(testDataTwo, 'name')).toStrictEqual(['joe', 'jill']);
+    });
 
-  test('buildList returns an array with one number', () => {
-    expect(buildList(testDataOne, 'age')).toStrictEqual([21]);
-  });
+    test('returns an array with one number', () => {
+      expect(buildList(testDataOne, 'age')).toStrictEqual([21]);
+    });
 
-  test('buildList returns an array with two numbers', () => {
-    expect(buildList(testDataTwo, 'age')).toStrictEqual([21, 15]);
+    test('returns an array with two numbers', () => {
+      expect(buildList(testDataTwo, 'age')).toStrictEqual([21, 15]);
+    });
   });
 });
