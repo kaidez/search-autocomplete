c6254925d56577e0baf9d2e582af49b40883990d
Kai Gittens <kai.gittens@gmail.com>
2022-09-12 17:37:37 -0400

    start getting eventListner tests working

---
+++ src/tests/index.test.ts
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+// import { showHideClearSearchButton } from '../index';
+
+describe('Search Field Clear button', () => {
+  test('should have its style property be "opacity: 0; cursor: none;"', () => {
+    document.body.innerHTML = `<div id="clearSearchButton" class="clearButton" title="Clear searches" style="opacity: 1; cursor: pointer;">×</div><input id="searchField" class="acronymInput" title="Search for an acronym" placeholder="your acronym" autofocus=""><section id="searchResults"><article>foo</article></section>`;
+
+    require('../index.ts');
+
+    const clearSearchStart = document.querySelector(
+      '#clearSearchButton'
+    ) as HTMLButtonElement;
+
+    const searchResults = document.querySelector(
+      '#searchResults'
+    ) as HTMLDivElement;
+
+    clearSearchStart.click();
+
+    const expectedFalseOpacityValue = '0';
+    const expectedFalseCursorValue = 'none';
+
+    expect(clearSearchStart.style.opacity).toBe(expectedFalseOpacityValue);
+
+    expect(clearSearchStart.style.cursor).toBe(expectedFalseCursorValue);
+  });
+});
